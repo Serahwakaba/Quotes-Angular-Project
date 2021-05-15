1faeffc64a9e5b3181ad2ab55c0daebcb00b6c04
Serah wakaba <serahwakaba9@gmail.com>
2021-05-15 10:31:02 +0300

    quotescomponents commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesComponent } from './quotes/quotes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesFormComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    QuotesComponent
   ],
   imports: [
     BrowserModule
